a296e90a3d00a566cc6bf625b661628df5aa791d
Alexandr Romanov <romanov.spb.alex@yandex.ru>
2019-10-07 16:33:01 +0300

    plans got real plans

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -8,6 +8,18 @@ import moment from 'moment';
 
 import { jsonstoreurl } from './../../hooks/useJSONStore.jsx';
 
+import {
+  fetchFromType,
+  RestaurantIcon,
+  SightseeingIcon,
+  HotelIcon,
+  PhotoplaceIcon,
+  FavouriteIcon,
+  NewplaceIcon
+} from './../Micro/CommonIcons.jsx';
+
+import { Manager, Notification } from './../Micro/Notifications.jsx';
+
 import { CircledAvatarSimple } from './../Micro/CircledAvatar.jsx';
 import { CityTimeFilter } from './../Micro/CityTimeFilter.jsx';
 
@@ -63,53 +75,6 @@ const InnerColumn = styled(Column)`
   height: 100%;
 `;
 
-const Marginer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 90%;
-  margin-left: 10%;
-`;
-
-const Numbers = styled(Column)`
-  justify-content: center;
-  text-align: center;
-  cursor: pointer;
-
-  h2 {
-    margin: 0;
-    padding: 0;
-    margin-bottom: 4px;
-    font-weight: 300;
-  }
-
-  small {
-    margin: 0;
-    padding: 0;
-    color: #69BCD5;
-  }
-`;
-
-const ShortBio = styled.p`
-  text-align: center;
-`;
-
-const Welcome = styled.h1`
-  margin: 0;
-  padding: 0;
-  margin-bottom: 1px;
-`;
-
-const UnderWelc = styled.small`
-  margin: 0;
-  padding: 0;
-  color: rgba(0,0,0,0.3);
-
-  u:hover {
-    color: #69BCD5;
-    cursor: pointer;
-  }
-`;
-
 const About = styled.div`
   width: 80%;
   margin-top: 20px;
@@ -133,28 +98,6 @@ const Quote = styled.p`
   padding: 0;
   padding-left: 38px;
   padding-right: 50px;
-`;
-
-const CenterUsername = styled.h4`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-`;
-
-const UsernameInput = styled.input`
-  outline: none;
-  border: none;
-  width: 70%;
-  font-size: 14pt;
-  text-align: center;
-
-  &::-webkit-contacts-auto-fill-button,
-  &::-webkit-credentials-auto-fill-button {
-    visibility: hidden;
-    pointer-events: none;
-    position: absolute;
-    right: 0;
-  }
 `;
 
 const ChatButton = styled.div`
@@ -313,106 +256,19 @@ const CloseAddMember = styled.small`
   color: rgba(0,0,0,0.3);
 `;
 
-const plans = [{
-  name: 'Eurotrip',
-  description: 'Краткое описание к планам, чтобы пользователи могли быстро ознакомиться с предстоящим путешествием до просмотра мест.',
-  members: [
-    {
-      id: 'rmnff'
-    }
-  ],
-  todo: [],
-  files: [],
-  to: '13 aug',
-  back: '24 aug',
-  countries: [{
-    name: 'Spain',
-    flag: 'sp',
-    cities: [
-      {
-        name: 'Barcelona',
-        arrival: '13 aug',
-        departure: '15 aug',
-        places: [{
-          type: 'Restaurant',
-          name: 'BBQ Inn, Meat-o-rant',
-          arrival: '12 pm',
-          open: '10 am',
-          close: '7 pm',
-          menu: 'link',
-          address: 'Nou de San Francesc 7',
-          comments: [],
-          image: 'https://tomesto.ru/img/place/000/022/585/restoran-brisket-bbq-brisket-na-smolenskom-bulvare_8e6c9_full-128780.jpg'
-        }, {
-          type: 'Sightseeing',
-          name: 'Kirche Sigrada Familia Barcelonar',
-          arrival: '2 pm',
-          open: null,
-          close: null,
-          menu: null,
-          address: 'Nou de Capitol',
-          comments: [],
-          image: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/6f/5d/13.jpg'
-        }, {
-          type: 'Sightseeing',
-          name: 'Barcelona Casa Batlo',
-          arrival: '4 pm',
-          open: null,
-          close: null,
-          menu: null,
-          address: 'Nou de Capitol',
-          comments: [],
-          image: 'https://cdn02.visitbarcelona.com/files/5531-3973-imagenCAT/BarcelonaGaudi-T24-d_O.jpg'
-        }, {
-          type: 'Restaurant',
-          name: 'Shisha palace, Shisha bar',
-          arrival: '6 pm',
-          open: '2 pm',
-          close: '01 am',
-          menu: 'link',
-          address: 'Nou de San Francesc 7',
-          comments: [],
-          image: 'https://media-cdn.tripadvisor.com/media/photo-s/04/b4/5f/2d/mono-shisha-bar.jpg'
-        }]
-      }
-    ]
-  }]
-}]
-
-const RestaurantIcon = () => (<i style={{ color: '#FF4040' }} className="fas fa-utensils"></i>)
-const SightseeingIcon = () => (<i style={{ color: '#32CD32' }} className="fas fa-monument"></i>)
-const HotelIcon = () => (<i style={{ color: '#1AA3FF' }} className="fas fa-bed"></i>)
-const PhotoplaceIcon = () => (<i style={{ color: '#6E7074' }} className="fas fa-camera-retro"></i>)
-const FavouriteIcon = () => (<i style={{ color: '#FFBF00' }} className="fas fa-star"></i>)
-const NewplaceIcon = () => (<i style={{ color: '#7EBCA1' }} className="fas fa-plus-circle"></i>)
-
-const fetchFromType = (type) => {
-  switch(type) {
-    case 'Restaurant':
-      return <RestaurantIcon />
-    case 'Sightseeing':
-      return <SightseeingIcon />
-    case 'Hotel':
-      return <HotelIcon />
-    case 'Photoplace':
-      return <PhotoplaceIcon />
-    case 'Favourite':
-      return <FavouriteIcon />
-    default:
-      break;
-  }
-}
-
 const Plans = (props) => {
 
   const [loading, setLoading] = useState(true);
-  // const [user, setUser] = useState(null);
   const [unauthorized, unauthorize] = useState(false);
-  // const [id] = useState(window.location.pathname.split('/')[2]);
-  const [fetchedPlan, setPlan] = useState(null);
+
   const [state, setState] = useState(null);
   const [stateUpdated, setStateUpdated] = useState(false);
+  const [fetchedPlan, setPlan] = useState(null);
+
+  const [selectedPlan, setSelectedPlan] = useState(0);
+  const [selectedCountry, setSelectedCountry] = useState(0);
   const [selectedCity, setSelectedCity] = useState(0);
+
   const [addMemberSelected, setAddMemberSelected] = useState(false);
   const [isAdmin, setIsAdmin] = useState(false);
 
@@ -427,14 +283,10 @@ const Plans = (props) => {
 
   useEffect(() => {
     if(!stateUpdated) return;
-    const self = window.localStorage.getItem('username')[0];
+    const self = window.localStorage.getItem('username');
     const fetched = state.plans.map(plan => {
-      if(plan.members.filter(member => member.id === self)) {
-        return plan
-      } else {
-        return null
-      }
-    })[0];
+      return plan.members.filter(member => member.id === self) ? plan : null;
+    }).filter(plan => plan != null)[selectedPlan];
     setPlan(fetched);
     setIsAdmin(fetched.members[0].id === self);
   }, [stateUpdated]);
@@ -453,11 +305,11 @@ const Plans = (props) => {
     e.preventDefault();
     const input = document.getElementById('memberUsername').value;
     const self = window.localStorage.getItem('username');
-    const alreadyMember = state.plans[0].members.filter(member => member.id === input)[0];
-    if(input === self || alreadyMember) return;
+    const alreadyMember = state.plans[selectedPlan].members.filter(member => member.id === input)[0];
+    if(input === self || alreadyMember) return Manager.warning('User is on the trip already', 'Whoops!', 2000);
     const found = state.users.filter(user => user.username === input)[0];
-    if(!found) return;
-    state.plans[0].members = state.plans[0].members.concat({ id: input });
+    if(!found) return Manager.warning('User with such username not found', 'Not found', 2000);
+    state.plans[selectedPlan].members = state.plans[selectedPlan].members.concat({ id: input });
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
     fetch(jsonstoreurl, {
@@ -465,44 +317,19 @@ const Plans = (props) => {
       headers: headers,
       body: JSON.stringify(state)
     }).then(response => response.json()).then(data => {
-      console.log(data);
+      if(data.ok) {
+        setStateUpdated(false);
+        setState(state);
+        setStateUpdated(true);
+        setAddMemberSelected(!addMemberSelected);
+        Manager.success('User joined the trip', 'So quick', 2000);
+      }
     });
   };
 
-  // const profile = async () => {
-  //   let url = 'https://85.143.216.19:3030/user/profile';
-  //   let config = {
-  //     method: 'GET',
-  //     mode: 'cors',
-  //     cache: 'no-cache',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Access-Control-Allow-Origin': '*'
-  //     },
-  //     redirect: 'follow',
-  //     referrer: 'no-referrer'
-  //   };
-  //   if(id) url = url + `/${id}`;
-  //   else config.body = JSON.stringify({
-  //     accessToken: window.localStorage.getItem('access-token')
-  //   });
-  //   const response = await fetch(url, config);
-  //   const data = await response.json();
-  //   const code = data.statusCode || 200;
-  //   if(code === 200) return data;
-  //   return null;
-  // };
+  const filterPlacesByType = (type) => {
 
-  // useEffect(() => {
-  //   if(user) return setLoading(false);
-  //   profile().then(data => {
-  //     if(!data && !id) unauthorize(true);
-  //     setUser(data);
-  //     setTimeout(() => {
-  //       setLoading(false);
-  //     }, 500);
-  //   });
-  // }, [user]);
+  }
 
   return(
     unauthorized ? <Redirect to={{ pathname: '/signin' }} /> : loading ? <WrapCentered>
@@ -514,18 +341,16 @@ const Plans = (props) => {
             <span style={{ color: 'skyblue', paddingTop: '15px', marginLeft: '8%' }}><NavLink to="/" style={{ textDecoration: 'none', color: 'skyblue' }}>{ '<–– Назад' }</NavLink></span>
             <Row style={{ width: '80%', marginTop: 20, marginLeft: '8%' }}>
               <Column>
-                <p style={{ margin: 0, color: 'rgba(0,0,0,0.5)' }}>13 август – 24 август</p>
-                <h1 style={{ margin: 0, marginTop: 16, fontSize: '36pt' }}>Eurotrip</h1>
+                <p style={{ margin: 0, color: 'rgba(0,0,0,0.5)' }}>{ fetchedPlan.to } – { fetchedPlan.back }</p>
+                <h1 style={{ margin: 0, marginTop: 16, fontSize: '36pt' }}>{ fetchedPlan.name }</h1>
                 <Row>
-                  <FlagIconCircled code='gb' />
-                  <FlagIconCircled code='es' />
-                  <FlagIconCircled code='pl' />
+                  { fetchedPlan.countries.map(country => <FlagIconCircled code={ country.flag } />) }
                 </Row>
               </Column>
             </Row>
             <About style={{ width: '80%', marginTop: 5, marginLeft: '8%', color: 'rgba(0,0,0,0.5)' }}>
               <LQuote>“</LQuote>
-              <Quote>Краткое описание к планам, чтобы пользователи могли быстро ознакомиться с предстоящим путешествием до просмотра мест.</Quote>
+              <Quote>{ fetchedPlan.description }</Quote>
               <RQuote>”</RQuote>
             </About>
             <Column>
@@ -598,20 +423,6 @@ const Plans = (props) => {
           </InnerColumn>
         </LeftColumn>
         <RightColumn>
-          {
-            // <InnerColumn>
-            //   <Column>
-            //     <Row style={{ width: '90%', marginLeft: '5%', borderBottom: '1px solid rgba(0,0,0,0.1)' }}>
-            //       <Row style={{ justifyContent: 'flex-start', marginTop: 15, marginBottom: 3 }}>
-            //         <p style={{ marginRight: 20, cursor: 'pointer', color: 'rgb(0, 158, 218)' }}>Tab 1</p>
-            //         <p style={{ marginRight: 20, cursor: 'pointer' }}>Tab 2</p>
-            //         <p style={{ marginRight: 20, cursor: 'pointer' }}>Tab 3</p>
-            //       </Row>
-            //     </Row>
-            //     { /* continue here */ }
-            //   </Column>
-            // </InnerColumn>
-          }
           <InnerColumn>
             <PlaceTypes>
               <Row>
@@ -625,7 +436,7 @@ const Plans = (props) => {
               </Row>
             </PlaceTypes>
             <PlaceItems>
-              <h3>{ fetchedPlan.cities[selectedCity].name }</h3>
+              <h3>{ fetchedPlan.countries[selectedCountry].cities[selectedCity].name }, { fetchedPlan.countries[selectedCountry].name }</h3>
               <Row style={{ width: '100%' }}>
                 <CityTimeFilter days={[{
                   date: '13 august',
@@ -635,7 +446,7 @@ const Plans = (props) => {
                   wname: 'Tuesday'
                 }]} />
                 <Column style={{ width: '85%' }}>
-                  { fetchedPlan.cities[selectedCity].places.map(place => {
+                  { fetchedPlan.countries[selectedCountry].cities[selectedCity].places.map(place => {
                       return (
                         <>
                           <PlaceItem>
@@ -672,6 +483,7 @@ const Plans = (props) => {
           <AddMemberButton onClick={ fetchUserAndAdd } >Добавить</AddMemberButton>
           <CloseAddMember onClick={ handleAddMember }>x</CloseAddMember>
         </AddMemberBar>
+        <Notification />
       </Wrap>
     )
   );
